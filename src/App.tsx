404b63410fc7872a5e5183afa62db36064d9ac76
IvanBanal <ibanal@rrc.ca>
2026-09-15 18:55:13 -0500

    Adding GameReviews component to App.tsx for testing purposes.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GameReviews } from "./pages/GameReviews";
+
+function App() {
+    return (
+        <>
+        {/* Commented this out for testing purposes.  */}
+            {/* <GameReviews /> */}
+        </>
+    );
+}
+
+export default App;
